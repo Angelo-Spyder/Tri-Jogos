8b8d1d3de4f014a06112681edec8ef153e376b30
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-06-10 19:50:46 -0300

    Adicionando imagem png de verdade e criando função de clisão e reset

--- primeiroJogo/sonico.js
+++ primeiroJogo/sonico.js
@@ -1,5 +1,6 @@
 
 const sonico = document.querySelector(".sonico");
+const espinho = document.querySelector(".espinhos")
 const game = document.querySelector(".game");
 
 game.addEventListener("click", function(event){
@@ -14,4 +15,21 @@ function pular(){
             sonico.classList.remove("pulo")
         },400)
     }
+}
+
+let estaVivo = setInterval(() =>{
+
+    //EIXO Y
+    let sonicoTop = parseInt(window.getComputedStyle(sonico).getPropertyValue("top"));
+    //EIXO X
+    let espinhoLeft = parseInt(window.getComputedStyle(espinho).getPropertyValue("left"));
+
+    if(espinhoLeft < 60 && espinhoLeft > 0 && sonicoTop >= 240){
+        alert("Tente novamente")
+        resetar()
+    }
+},10)
+
+function resetar(){
+    espinhoLeft = 570
 }
